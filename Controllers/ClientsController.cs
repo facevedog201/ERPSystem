59619f62a552eefe9f652e0ba87ca08c235802c9
CodeRoad <CodeRoad@DESKTOP-8KA7PS7>
2025-10-11 20:45:15 -0600

    cards para las vistas de clientes

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -36,7 +36,7 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> InactiveClients()
         {
             var inactiveClients = await _context.Clients
-                                                .Where(c => !c.IsActive==false)
+                                                .Where(c => c.IsActive != true)
                                                 .ToListAsync();
             return View(inactiveClients);
         }
@@ -45,7 +45,7 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> Details(int id)
         {
             var client = await _context.Clients.FindAsync(id);
-            if (client == null || !client.IsActive==false) return NotFound();
+            if (client == null || client.IsActive != true) return NotFound();
             return View(client);
         }
 
@@ -76,7 +76,7 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> Edit(int id)
         {
             var client = await _context.Clients.FindAsync(id);
-            if (client == null || !client.IsActive == false) return NotFound();
+            if (client == null || !(client.IsActive ?? false)) return NotFound();
             return View(client);
         }
 
@@ -87,7 +87,7 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> Edit(Client client)
         {
             var existingClient = await _context.Clients.FindAsync(client.ClientId);
-            if (existingClient == null || !existingClient.IsActive == false) return NotFound();
+            if (existingClient == null || !(existingClient.IsActive ?? false)) return NotFound();
 
             if (ModelState.IsValid)
             {
@@ -107,7 +107,7 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> Delete(int id)
         {
             var client = await _context.Clients.FindAsync(id);
-            if (client == null || !client.IsActive == false) return NotFound();
+            if (client == null || (client.IsActive ?? false) == false) return NotFound();
             return View(client);
         }
 
@@ -118,7 +118,7 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var client = await _context.Clients.FindAsync(id);
-            if (client == null || !client.IsActive == false) return NotFound();
+            if (client == null || !(client.IsActive ?? false)) return NotFound();
 
             client.IsActive = false; // soft delete
             await _context.SaveChangesAsync();
@@ -133,114 +133,13 @@ namespace ERPSystem.Controllers
         public async Task<IActionResult> Activate(int id)
         {
             var client = await _context.Clients.FindAsync(id);
-            if (client == null || client.IsActive==false) return NotFound();
+            if (client == null || (client.IsActive ?? false)) return NotFound();
 
             client.IsActive = true;
             await _context.SaveChangesAsync();
             _auditService.Log("Activate", "Client", client.ClientId, $"Se reactivó el Cliente {client.ClientId}");
-            return RedirectToAction(nameof(InactiveClients));
+            return RedirectToAction(nameof(Index));
         }
     }
 }
 
-
-
-
-
-//using ERPSystem.Data;
-//using ERPSystem.Helpers;
-//using ERPSystem.Models;
-//using ERPSystem.Services;
-//using Microsoft.AspNetCore.Mvc;
-//using System.Linq;
-
-
-//namespace ERPSystem.Controllers
-//{
-//    [RoleAuthorize("Admin","Recepcion","Contabilidad")]
-//    public class ClientsController : Controller
-//    {
-//        private readonly AppDbContext _context;
-//        private readonly AuditService _auditService;
-
-//        public ClientsController(AppDbContext context, AuditService auditService)
-//        {
-//            _context = context;
-//            _auditService = auditService;
-
-//        }
-
-//        // GET: /Clients
-//        public IActionResult Index()
-//        {
-//            var clients = _context.Clients.ToList();
-//            return View(clients);
-//        }
-
-//        // GET: /Clients/Create
-//        public IActionResult Create()
-//        {
-//            return View();
-//        }
-
-//        // POST: /Clients/Create
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public IActionResult Create(Client client)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                _context.Clients.Add(client);
-//                _context.SaveChanges();
-//                _auditService.Log("Create", "Client", client.ClientId,$"Se creó el Cliente {client.ClientId}");
-//                return RedirectToAction(nameof(Index));
-//            }
-//            return View(client);
-//        }
-
-//        // GET: /Clients/Edit/5
-//        public IActionResult Edit(int id)
-//        {
-//            var client = _context.Clients.Find(id);
-//            if (client == null) return NotFound();
-//            return View(client);
-//        }
-
-//        // POST: /Clients/Edit/5
-//        [HttpPost]
-//        [ValidateAntiForgeryToken]
-//        public IActionResult Edit(Client client)
-//        {
-//            if (ModelState.IsValid)
-//            {
-//                _context.Clients.Update(client);
-//                _context.SaveChanges();
-//                _auditService.Log("Edit", "Client", client.ClientId, $"Se edito el Cliente {client.ClientId}");
-//                return RedirectToAction(nameof(Index));
-//            }
-//            return View(client);
-//        }
-
-//        // GET: /Clients/Delete/5
-//        public IActionResult Delete(int id)
-//        {
-//            var client = _context.Clients.Find(id);
-//            if (client == null) return NotFound();
-//            return View(client);
-//        }
-
-//        // POST: /Clients/Delete/5
-//        [HttpPost, ActionName("Delete")]
-//        [ValidateAntiForgeryToken]
-//        public IActionResult DeleteConfirmed(int id)
-//        {
-//            var client = _context.Clients.Find(id);
-//            if (client == null) return NotFound();
-
-//            _context.Clients.Remove(client);
-//            _context.SaveChanges();
-//            _auditService.Log("Delete", "Client", client.ClientId, $"Se elimino el Cliente {client.ClientId}");
-//            return RedirectToAction(nameof(Index));
-//        }
-//    }
-//}
